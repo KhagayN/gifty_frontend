569515286110ffc4e9f958997f5670da556be01c
Khagay Nagdimov <khagay@nyu.edu>
2021-11-23 16:06:16 -0500

    updata

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import * as Survey from "survey-react";
+import { React, Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  //Define Survey JSON
+  //Here is the simplest Survey with one text question
+  json = {
+   elements: [
+    { type: "text", name: "customerName", title: "What is your name?", isRequired: true}
+   ]
+  };
+ 
+  //Define a callback methods on survey complete
+  onComplete(survey, options) {
+   //Write survey results into database
+   console.log("Survey results: " + JSON.stringify(survey.data));
+  }
+  render() {
+   //Create the model and pass it into react Survey component
+   //You may create survey model outside the render function and use it in your App or component
+   //The most model properties are reactive, on their change the component will change UI when needed.
+   var model = new Survey.Model(this.json);
+   return (<Survey.Survey model={model} onComplete={this.onComplete}/>);
+   /*
+   //The alternative way. react Survey component will create survey model internally
+   return (<Survey.Survey json={this.json} onComplete={this.onComplete}/>);
+   */
+   //You may pass model properties directly into component or set it into model
+   // <Survey.Survey model={model} mode="display"/>
+   //or 
+   // model.mode="display"
+   // <Survey.Survey model={model}/>
+   // You may change model properties outside render function. 
+   //If needed react Survey Component will change its behavior and change UI.
+  }
+ } 
 
 export default App;
