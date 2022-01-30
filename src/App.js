40b0cb15c5ed42ceb58ed1b5063593a2ac9f4ae9
Khagay Nagdimov <kh.guy@Hotmail.com>
2022-01-29 22:00:26 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import Form from './Form'
 
 class App extends Component {
 
-  gifty_backend_url = "https://gifty-backend.ue.r.appspot.com"
-  
   render() {
-   fetch(this.gifty_backend_url)
-   .then(response => response.json())  
-   .then(data => console.log(data));
-
    return (<Form/>);
-
   }
  }
 
